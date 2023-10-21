0f3a5c726c4d1370bb4f8289cab701f03998fa4e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-10-21 16:28:20 -0500

    Rearranged functions and added prototypes

--- main.cpp
+++ main.cpp
@@ -5,12 +5,92 @@
 #include <sstream>
 using namespace std;
 
+/*Struct that stores data about the matrices read from the given file.*/
 struct Data
 {
     int size;
     int **matrix_1;
     int **matrix_2;
 };
+
+// Function Prototypes (I tried to put the struct here as well, but it is required for where it is.)
+Data matrixLoad(string filename);
+int **matrixAdd(Data data);
+int **matrixSubtract(Data data);
+int **matrixProduct(Data data);
+
+int main() {
+    //--HEADING--//
+    cout << "Riley Meyerkorth" << endl;
+    cout << "Lab #6: Matrix Manipulation" << endl;
+
+    //--STEP 1--//
+    // Load matrices from file
+    Data data = matrixLoad("matrix.txt");
+    int size = data.size;
+    int** matrix_1 = data.matrix_1;
+    int** matrix_2 = data.matrix_2;
+    cout << "Matrix Sizes: " << size << " x " << size << endl;
+
+    //--STEP 2--//
+    cout << "\nMatrix A" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", matrix_1[i][j]);
+        }
+        printf("\n");
+    }
+    cout << "Matrix B" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", matrix_2[i][j]);
+        }
+        printf("\n");
+    }
+
+    //--STEP 3--//
+    cout << "Matrix Sum (A + B)" << endl;
+    int **addedMatrix = matrixAdd(data);
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", addedMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    //--STEP 4--//
+    cout << "Matrix Product (A * B)" << endl;
+    int **productMatrix = matrixProduct(data);
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", productMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    //--STEP 5--//
+    cout << "Matrix Difference (A - B)" << endl;
+    int **subtractedMatrix = matrixSubtract(data);
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%5i", subtractedMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Return from main
+    return 0;
+}
 
 /* Loads 2 matrices in from a text file and returns an array containing the size and both matrices*/
 Data matrixLoad(string filename) {
@@ -147,77 +227,4 @@ int **matrixProduct(Data data)
     }
 
     return result;
-}
-
-int main() {
-    //--HEADING--//
-    cout << "Riley Meyerkorth" << endl;
-    cout << "Lab #6: Matrix Manipulation" << endl;
-
-    //--STEP 1--//
-    // Load matrices from file
-    Data data = matrixLoad("matrix.txt");
-    int size = data.size;
-    int** matrix_1 = data.matrix_1;
-    int** matrix_2 = data.matrix_2;
-    cout << "Matrix Sizes: " << size << " x " << size << endl;
-
-    //--STEP 2--//
-    cout << "\nMatrix A" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%5i", matrix_1[i][j]);
-        }
-        printf("\n");
-    }
-    cout << "Matrix B" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%5i", matrix_2[i][j]);
-        }
-        printf("\n");
-    }
-
-    //--STEP 3--//
-    cout << "Matrix Sum (A + B)" << endl;
-    int **addedMatrix = matrixAdd(data);
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%5i", addedMatrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    //--STEP 4--//
-    cout << "Matrix Product (A * B)" << endl;
-    int **productMatrix = matrixProduct(data);
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%5i", productMatrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    //--STEP 5--//
-    cout << "Matrix Difference (A - B)" << endl;
-    int **subtractedMatrix = matrixSubtract(data);
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%5i", subtractedMatrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    // Return from main
-    return 0;
 }
